ec48ababe13886b193f20b768724153564545211
RenanMafort <94027729+RenanMafort@users.noreply.github.com>
2024-05-06 22:52:26 -0300

    Update messages.service.ts

--- src/app/services/messages.service.ts
+++ src/app/services/messages.service.ts
@@ -20,30 +20,6 @@ export class MessagesService {
   clear(){
     this.message = '';
   }
-  // Exemplo de código JavaScript com vulnerabilidades
 
-// 1. Exemplo de XSS (Cross-Site Scripting)
- welcomeUser() {
-    var name = document.getElementById('name').value;
-    document.getElementById('welcome-message').innerHTML = 'Bem-vindo, ' + name + '!';
-}
-
-// 2. Exemplo de Injeção de SQL
- searchProducts() {
-    var searchTerm = document.getElementById('search-term').value;
-    var sqlQuery = "SELECT * FROM produtos WHERE nome = '" + searchTerm + "'";
-    // Executar a consulta SQL...
-}
-
-// 3. Exemplo de Validação de Entrada Insuficiente
- authenticateUser() {
-    var username = document.getElementById('username').value;
-    var password = document.getElementById('password').value;
-    if (username === 'admin' && password === 'admin') {
-        // Autenticar o usuário...
-    } else {
-        // Exibir mensagem de erro...
-    }
-}
 
 }
